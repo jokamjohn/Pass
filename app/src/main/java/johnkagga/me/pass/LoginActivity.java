2aa03e66a5231c77caa85118221b7a3cc41869f4
johnkagga <johnkagga@gmail.com>
2015-12-20 01:35:02 +0300

    added signup activity

--- app/src/main/java/johnkagga/me/pass/LoginActivity.java
+++ app/src/main/java/johnkagga/me/pass/LoginActivity.java
@@ -1,10 +1,15 @@
 package johnkagga.me.pass;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    protected TextView mSignupText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,7 +18,20 @@ public class LoginActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        initializeScreen();
 
+        mSignupText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private void initializeScreen() {
+        mSignupText = (TextView) findViewById(R.id.signupText);
     }
 
 }
