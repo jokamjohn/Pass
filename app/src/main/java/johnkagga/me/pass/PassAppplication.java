507ed227d452fa0becc992da017361ad293cd85a
johnkagga <johnkagga@gmail.com>
2015-12-20 22:10:37 +0300

    Connected parse backend

---
+++ app/src/main/java/johnkagga/me/pass/PassAppplication.java
@@ -0,0 +1,19 @@
+package johnkagga.me.pass;
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+public class PassAppplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // [Optional] Power your app with Local Datastore. For more info, go to
+        // https://parse.com/docs/android/guide#local-datastore
+        Parse.enableLocalDatastore(this);
+
+        Parse.initialize(this);
+
+    }
+}
