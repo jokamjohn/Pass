c9f9b2823b426765c90ad5cc66bff36691ee58b2
johnkagga <johnkagga@gmail.com>
2015-12-22 13:51:18 +0300

    Adding a ParseRelation

--- app/src/main/java/johnkagga/me/pass/EditFriendsActivity.java
+++ app/src/main/java/johnkagga/me/pass/EditFriendsActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.ProgressBar;
@@ -12,7 +13,9 @@ import android.widget.ProgressBar;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
+import com.parse.ParseRelation;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import java.util.List;
 
@@ -23,6 +26,8 @@ public class EditFriendsActivity extends AppCompatActivity {
     protected ListView mListView;
     protected List<ParseUser> mUsers;
     protected ProgressBar mProgressBar;
+    protected ParseRelation<ParseUser> mUserParseRelation;
+    protected ParseUser mCurrentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +42,25 @@ public class EditFriendsActivity extends AppCompatActivity {
         //Select the clicked item
         mListView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
 
+        //When an item is selected
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //Add relation and save
+                mUserParseRelation.add(mUsers.get(position));
+                mCurrentUser.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e != null)
+                        {
+                            //error occurred
+                            Log.e(LOG_TAG,e.getMessage());
+                        }
+                    }
+                });
+            }
+        });
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
@@ -44,7 +68,15 @@ public class EditFriendsActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
 
+        //Get current data at all times
+        //Get current user
+        mCurrentUser = ParseUser.getCurrentUser();
+        //Add friends to the current user
+        mUserParseRelation = mCurrentUser.getRelation(ParseConstants.KEY_USER_RELATION);
+
+        //Show progress bar
         mProgressBar.setVisibility(View.VISIBLE);
+
         //Query for users
         ParseQuery<ParseUser> query = ParseUser.getQuery();
         query.orderByAscending(ParseConstants.KEY_USERNAME);
@@ -69,12 +101,14 @@ public class EditFriendsActivity extends AppCompatActivity {
 
                 } else {
                     Log.i(LOG_TAG, e.getMessage());
-                    String title = "Sorry";
+                    String title = getString(R.string.query_friends_dialog_title);
                     String message = e.getMessage();
+                    //Make an Alert Dialog
                     Helper.alertDialog(EditFriendsActivity.this, title, message);
                 }
             }
         });
     }
+
 
 }
--- app/src/main/java/johnkagga/me/pass/ParseConstants.java
+++ app/src/main/java/johnkagga/me/pass/ParseConstants.java
@@ -7,8 +7,9 @@ public final class ParseConstants {
 
     //Class constants
 
-    //Field constants
+    //Key constants
     public static final String KEY_USERNAME = "username";
     public final static int USER_LIMIT = 1000;
+    public final static String KEY_USER_RELATION = "friends";
 }
 
