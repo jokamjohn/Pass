ea438a8a9b4b43c00716420f43b0a34d3f71b9b0
johnkagga <johnkagga@gmail.com>
2015-12-25 15:05:05 +0300

    Set a timer, image only viewed for 10 seconds only

--- app/src/main/java/johnkagga/me/pass/ViewImageActivity.java
+++ app/src/main/java/johnkagga/me/pass/ViewImageActivity.java
@@ -3,13 +3,14 @@ package johnkagga.me.pass;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class ViewImageActivity extends AppCompatActivity {
 
@@ -26,17 +27,19 @@ public class ViewImageActivity extends AppCompatActivity {
         //Using Piccaso lib
         Picasso.with(this).load(fileUri.toString()).into(imageView);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        //Timer and task
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void run() {
+                finish();
             }
-        });
+        }, 10 * 1000);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
+
 
     }
 
