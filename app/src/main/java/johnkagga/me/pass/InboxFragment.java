cdd7eb3428ec56650e1b480c75c78dca6cc4d5c8
johnkagga <johnkagga@gmail.com>
2015-12-25 11:14:29 +0300

    Showing the sender name in the inbox

--- app/src/main/java/johnkagga/me/pass/InboxFragment.java
+++ app/src/main/java/johnkagga/me/pass/InboxFragment.java
@@ -2,20 +2,66 @@ package johnkagga.me.pass;
 
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.List;
 
 /**
  * Created by jokamjohn on 12/21/2015.
  */
 public class InboxFragment extends ListFragment {
 
+    private static final String LOG_TAG = InboxFragment.class.getSimpleName();
+    protected List<ParseObject> mMessages;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_inbox, container, false);
 
         return rootView;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        ParseQuery<ParseObject> query = new ParseQuery<>(ParseConstants.CLASS_MESSAGE);
+        query.whereEqualTo(ParseConstants.KEY_RECIPIENTS_IDS, ParseUser.getCurrentUser().getObjectId());
+        query.addAscendingOrder(ParseConstants.KEY_CREATED_AT);
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> messages, ParseException e) {
+                if (e == null)
+                {
+                    //success
+                    mMessages = messages;
+                    String[] senderName = new String[messages.size()];
+                    int i = 0;
+                    for (ParseObject message : mMessages)
+                    {
+                        senderName[i] = message.getString(ParseConstants.KEY_SENDER_NAME);
+                        i++;
+                    }
+
+                    ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(),
+                            android.R.layout.simple_list_item_1, senderName);
+                    setListAdapter(adapter);
+                }
+                else {
+                    //Error
+                    Log.v(LOG_TAG,e.getMessage());
+                }
+            }
+        });
     }
 }
--- app/src/main/java/johnkagga/me/pass/ParseConstants.java
+++ app/src/main/java/johnkagga/me/pass/ParseConstants.java
@@ -19,6 +19,7 @@ public final class ParseConstants {
     public static final String KEY_SENDER_NAME = "senderName";
     public static final String KEY_FILE = "file";
     public static final String KEY_FILE_TYPE = "fileType";
+    public static final String KEY_CREATED_AT = "createdAt";
 
     //Application global constants
     public static final String IMAGE_TYPE ="image";
